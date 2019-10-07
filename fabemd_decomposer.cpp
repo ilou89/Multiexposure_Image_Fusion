c5487e0374eb1336c94584f6bd2e1164e598f52c
Ilias <ilousis89@gmail.com>
2019-10-07 22:50:24 +0300

    create ycbcr channels

--- fabemd_decomposer.cpp
+++ fabemd_decomposer.cpp
@@ -8,7 +8,9 @@ fabemd_decomposer::fabemd_decomposer()
 
 void fabemd_decomposer::getIMFs()
 {
-    //Step 1: convert to YCbCr
+    //Step 1: Clear memory from previous execution
+    //TODO
+    //Step 2: convert to YCbCr
     rgb_to_ycbcr();
     //Step2: TODO
 }
@@ -20,17 +22,42 @@ void fabemd_decomposer::setInputImages(QVector<QImage *> *images)
 
 void fabemd_decomposer::rgb_to_ycbcr()
 {
-    //Parse input images, convert them to YCbCr and save each channel to the respective vector
+    //parse input images, convert them to YCbCr and save each channel to the respective vector
     for(int k = 0; k < inputImages->length(); ++k){
-        QVector<QVector<float>> *y_channel = new QVector<QVector<float> >(inputImages->at(k)->width(), QVector<float>(inputImages->at(k)->height()));
-        qDebug()<<y_channel->length()<<y_channel->at(0).length()<<sizeof (y_channel);
+        const int RESX = static_cast<int>(inputImages->at(k)->width());
+        const int RESY = static_cast<int>(inputImages->at(k)->height());
+        //allocate memory
+        //TODO create separate function that performs memory allocation
+        float **y_channel;
+        float **cb_channel;
+        float **cr_channel;
+        y_channel  = new float*[static_cast<uint>(RESX)];
+        cb_channel = new float*[static_cast<uint>(RESX)];
+        cr_channel = new float*[static_cast<uint>(RESX)];
+        for(int i = 0; i < RESX; ++i){
+            y_channel[i]  = new float[static_cast<uint>(RESY)];
+            cb_channel[i] = new float[static_cast<uint>(RESY)];
+            cr_channel[i] = new float[static_cast<uint>(RESY)];
+        }
 
-        for(int i = 0; i < y_channel->length(); ++i){
-            for(int j = 0; j < y_channel->at(i).length(); ++j){
-//                *y_channel[i][j] = bla bla;
+        //convert to ycbcr format
+        for(int i = 0; i < RESX; ++i){
+            for(int j = 0; j < RESY; ++j){
+                QColor rgb = inputImages->at(k)->pixel(i, j);
+                float red   = static_cast<float>(rgb.red());
+                float green = static_cast<float>(rgb.green());
+                float blue  = static_cast<float>(rgb.blue());
+
+                y_channel[i][j]  =  16.f +  65.738f*red/256.f + 129.057f*green/256.f +  25.064f*blue/256.f;
+                cb_channel[i][j] = 128.f -  37.945f*red/256.f -  74.494f*green/256.f + 112.439f*blue/256.f;
+                cr_channel[i][j] = 128.f + 112.439f*red/256.f -  94.154f*green/256.f -  18.285f*blue/256.f;
             }
         }
 
+        //push channels to lists (vectors)
+        y_channels.push_back(&y_channel);
+        cb_channels.push_back(&cb_channel);
+        cr_channels.push_back(&cr_channel);
     }
 }
 
--- fabemd_decomposer.h
+++ fabemd_decomposer.h
@@ -8,12 +8,11 @@ class fabemd_decomposer
 private:
     QVector<QImage*> *inputImages;
     //2D arrays of size (width x height)
-    QVector<QVector<QVector<float>>> y_channels;
-    QVector<QVector<QVector<float>>> cb_channels;
-    QVector<QVector<QVector<float>>> cr_channels;
-
-
+    QVector<float***> y_channels;
+    QVector<float***> cb_channels;
+    QVector<float***> cr_channels;
     void rgb_to_ycbcr();
+
 public:
     fabemd_decomposer();
 
